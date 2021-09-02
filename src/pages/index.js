d31976bf7a63c9ef74371504c7371ef9b0e64692
Giosepe Luiz <giosepe_luiz3@live.com>
2021-09-02 05:07:55 -0300

    Adição de variáveis e comandos SCSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { NextSeo } from "next-seo";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -35,35 +35,16 @@ export default function Home() {
 
       {/* Conteúdo principal */}
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Boilerplate de NextJs</h1>
 
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
+        <p className={styles.description}>Template padrão para seu projeto NextJs</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://github.com/giosepeluiz/boilerplate-nextjs/blob/main/README.md"
             className={styles.card}>
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+            <h2>Leia as informações &rarr;</h2>
+            <p>Para saber como utilizar todos os recursos que o modelo oferece.</p>
           </a>
         </div>
       </main>
@@ -73,7 +54,7 @@ export default function Home() {
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer">
-          Powered by{" "}
+          Elaborado por Giosepe Luiz | Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vectors/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
